928ff03eecd087b9248af0f99953973c2b8dbffe
Marc Hibbins <marchibbins@gmail.com>
2015-07-14 15:19:53 +0100

    Setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+// Setup
+var gulp = require('gulp'),
+    del = require('del'),
+    minifycss = require('gulp-minify-css'),
+    notify = require('gulp-notify'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-ruby-sass');
+
+// Styles
+gulp.task('styles', function() {
+    return sass('sass/main.scss', {
+        style: 'expanded'
+    })
+    .pipe(gulp.dest('css'))
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('css'))
+    .pipe(notify({
+        message: 'Styles task complete'
+    }));
+});
+
+// Clean
+gulp.task('clean', function(cb) {
+    del(['css'], cb)
+});
+
+// Watch
+gulp.task('watch', function() {
+    gulp.watch('sass/**/*.scss', ['styles']);
+});
+
+// Default
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles');
+});
